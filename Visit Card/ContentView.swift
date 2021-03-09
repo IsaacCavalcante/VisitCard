b1a646e13e052a7aee0e277733cf956b17b40bfa
Isaac <cavalcante.isaac1@gmail.com>
2021-03-09 17:39:38 -0300

    Mudando cor de fundo

--- Visit Card/ContentView.swift
+++ Visit Card/ContentView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack {
+            Color(red: 0.17, green: 0.24, blue: 0.31, opacity: 1.00)
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
             Text("Hello, world!")
                 .padding()
         }
