e0030269601220062fd6997b72573ca6b049995e
Isaac <cavalcante.isaac1@gmail.com>
2021-03-09 14:42:28 -0300

    Initial Commit

---
+++ Visit Card/Visit_CardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Visit_CardApp.swift
+//  Visit Card
+//
+//  Created by Isaac Cavalcante on 09/03/21.
+//
+
+import SwiftUI
+
+@main
+struct Visit_CardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
